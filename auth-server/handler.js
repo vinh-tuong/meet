d6d61c84f53d857722d71b951f746e666c0111db
KostasRafael <korafamanou@gmail.com>
2024-08-07 18:39:23 +0300

    some changes

---
+++ auth-server/handler.js
@@ -0,0 +1,66 @@
+"use strict";
+
+const { google } = require("googleapis");
+const calendar = google.calendar("v3");
+const SCOPES = [
+  "https://www.googleapis.com/auth/calendar.events.public.readonly",
+];
+const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
+const redirect_uris = ["https://kostasrafael.github.io/meet/"];
+
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  redirect_uris[0]
+);
+
+module.exports.getAuthURL = async () => {
+  // when this function is invoked, an authorization URL is returned, and Google displays a consent screen to the
+  // user to authorize the app via an authorization code.
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
+  });
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    body: JSON.stringify({
+      authUrl,
+    }),
+  };
+};
+
+module.exports.getAccessToken = async (event) => {
+  // a request will only be made to this function if the user approves. The authorization code received from
+  // 'getAuthURL' is passed to getAccessToken. Google subsequently provides the meet app with a temporary access token.
+  const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+  return new Promise((resolve, reject) => {
+    return oAuth2Client.getToken(code, (error, response) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(response);
+    });
+  })
+    .then((results) => {
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
+        body: JSON.stringify(results),
+      };
+    })
+    .catch((error) => {
+      return {
+        statusCode: 500,
+        body: JSON.stringify(error),
+      };
+    });
+};
